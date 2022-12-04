9046a938df4c87d4bb513d095775129fb9113a01
sunriver <seriousman1994217winter@gmail.com>
2022-12-04 18:01:24 +0400

    add css and global css and default layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,23 @@
 import Link from "next/link";
+import Head from 'next/head';
+import layout from "../../components/layout";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+                <script src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={ () => {
+                    console.log(`script loaded correctly, window.F8 has been populated`)
+                }}></script>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href="/">Back to Home</Link>
             </h2>
-        </>
+        </Layout>
     )
 }
