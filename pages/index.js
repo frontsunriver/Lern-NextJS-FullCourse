f60f2157a87ebe13a37c6f182fe5756ef38b7bb3
sunriver <seriousman1994217winter@gmail.com>
2022-12-04 10:38:49 +0400

    initial project

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">This page</Link>
         </h1>
 
         <p className={styles.description}>
